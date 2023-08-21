7ec6ac380224bd20a4197ca7a030b6973dfd5e71
steamwebapi <132143851+steamwebapi@users.noreply.github.com>
2023-08-21 19:18:26 +0200

    Update inventoryItem.jsx
    Update Property changes

--- components/inventoryItem.jsx
+++ components/inventoryItem.jsx
@@ -4,14 +4,14 @@ function InventoryItem(props) {
     return <div className="w-full lg:w-1/5">
         <div className="card bg-gray-900 shadow-xl m-2 rounded">
             <figure className="w-full h-[200px] flex flex-center">
-                <img src={props.item.itemImages[0]} alt="Inventory Image" className="max-w-[200px]"/></figure>
+                <img src={props.item.itemimages[0]} alt="Inventory Image" className="max-w-[200px]"/></figure>
             <div className="card-body">
                 <h2 className="card-title text-base h-[60px] flex flex-start items-start text-white">
-                    {props.item.marketName}
+                    {props.item.markethashname}
                 </h2>
                 <div className="card-actions justify-between text-gray-400 flex items-center">
-                    <div className="text-xl uppercase text-white font-bold">{props.item.priceLatest}$</div>
-                    <div className="badge badge-outline p-4 uppercase">{props.item.itemGroup}</div>
+                    <div className="text-xl uppercase text-white font-bold">{props.item.pricelatest}$</div>
+                    <div className="badge badge-outline p-4 uppercase">{props.item.itemgroup}</div>
                 </div>
             </div>
         </div>
